029138835ccfb193ed7a7af9328769895e76ecb7
dhkehd2 <dhkehd2@gmail.com>
2023-06-23 00:15:19 +0900

    아폴로 샌드박스 - 플래이그라운드 보다 편리한 기능, UI 클릭으로 원하는 필드 query & mutation 셋팅 가능. - docs : https://www.apollographql.com/blog/announcement/platform/apollo-sandbox-an-open-graphql-ide-for-local-development/

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      playground: true, // 플레이 그라운드 활성화 여부
+      playground: false, // 플레이 그라운드 활성화 여부
       autoSchemaFile: 'src/common/graphql/schema.gql',
+      sortSchema: true, // 포함된 모듈에 정의된 순서대로 스키마 정렬
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
   ],
   providers: [AppResolver, AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GraphQLSchemaHost } from '@nestjs/graphql';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  const { schema } = app.get(GraphQLSchemaHost);
+  console.log(schema);
 }
 bootstrap();
